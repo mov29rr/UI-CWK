44ad605be00b9dd680845f7b15ed10a5c6c89736
naputt jarungpolpipat <naputt@naputts-Laptop.local>
2024-12-06 15:43:54 +0000

    app init database

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -1,33 +1,40 @@
 #include "application.hpp"
 
 Application::Application(int argc, char** argv)
-    : _handle(std::make_unique<QApplication>(argc, argv))
-    , _window(std::make_unique<Window>())
-{
-    try
-    {
-        // Reading stylesheet from file
-        QFile file(STYLE_SHEET_FILE_PATH);
-        if(!file.exists())
-        {
-            throw std::runtime_error("Failed to open stylesheet");
-        }
-        
-        file.open(QFile::ReadOnly);
-        auto contents = QLatin1String(file.readAll());
+    : _handle(std::make_unique<QApplication>(argc, argv)), _window(std::make_unique<Window>()) {
+  try {
+    m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db.setDatabaseName("../database/database.sqlite");
+    if (!m_db.open()) {
+      qDebug() << "Failed to open database:";
+    }
 
-        _handle->setStyleSheet(contents);
-        
-        _window->show();
+    // Reading stylesheet from file
+    QFile file(STYLE_SHEET_FILE_PATH);
+    if (!file.exists()) {
+      throw std::runtime_error("Failed to open stylesheet");
     }
-    catch(const std::exception& exception)
-    {
-        std::cerr << "\033[0;31mError\033[0m: " << exception.what() << std::endl;
-        _errored = true;
-    }
+
+    file.open(QFile::ReadOnly);
+    auto contents = QLatin1String(file.readAll());
+
+    _handle->setStyleSheet(contents);
+
+    _window->show();
+
+    // QSqlQuery query;
+    // query.prepare("SELECT ID FROM site LIMIT :LIMIT");
+    // query.bindValue(":LIMIT", 20);
+    // if (query.exec()) {
+    //   while (query.next()) {
+    //     std::cout << query.value(0).toInt() << std::endl;
+    //   }
+    // }
+
+  } catch (const std::exception& exception) {
+    std::cerr << "\033[0;31mError\033[0m: " << exception.what() << std::endl;
+    _errored = true;
+  }
 }
 
-int Application::exec()
-{
-    return _errored ? EXIT_FAILURE : _handle->exec();
-}
+int Application::exec() { return _errored ? EXIT_FAILURE : _handle->exec(); }
--- src/core/application.hpp
+++ src/core/application.hpp
@@ -1,35 +1,39 @@
 #pragma once
 
-#include "window.hpp"
-
-#include <memory>
-#include <sstream>
+#include <QSqlDatabase>
+#include <QSqlQuery>
 #include <fstream>
 #include <iostream>
+#include <memory>
+#include <sstream>
+
+#include "window.hpp"
 
 /**
  * The application class
  */
-class Application
-{
-private:
-    static constexpr const char* STYLE_SHEET_FILE_PATH = "stylesheets/stylesheet.qss";
+class Application {
+ private:
+  static constexpr const char* STYLE_SHEET_FILE_PATH = "stylesheets/stylesheet.qss";
 
-    std::unique_ptr<QApplication> _handle;
-    std::unique_ptr<Window> _window;
+  std::unique_ptr<QApplication> _handle;
+  std::unique_ptr<Window> _window;
 
-    bool _errored = false;
-public:
-    /**
-     * Constructs the application
-     * 
-     * @param argc the argument count.
-     * @param argv the argument vector.
-     */
-    Application(int argc, char** argv);
+  bool _errored = false;
 
-    /**
-     * Executes the application
-     */
-    int exec();
+  QSqlDatabase m_db;
+
+ public:
+  /**
+   * Constructs the application
+   *
+   * @param argc the argument count.
+   * @param argv the argument vector.
+   */
+  Application(int argc, char** argv);
+
+  /**
+   * Executes the application
+   */
+  int exec();
 };
