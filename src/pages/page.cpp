4870ffdb6d494309d3e71e4e72b700e8b56ffadf
Leyna Tiji [sc23lt] <sc23lt@feng-linux-07.leeds.ac.uk>
2024-12-08 21:14:29 +0000

    fixed layout of pollutant page

--- src/pages/page.cpp
+++ src/pages/page.cpp
@@ -1,18 +1,12 @@
 #include "page.hpp"
 
-Page::Page(const char* title)
-    : title(title)
-    , layout(new QVBoxLayout)
-{
-    layout->setAlignment(Qt::AlignTop);
+Page::Page(const char* title) : title(title), layout(new QVBoxLayout) {
+  layout->setAlignment(Qt::AlignTop);
 
-    auto header = new QLabel(title);
-    header->setObjectName("header");
-    layout->addWidget(header);
-    layout->setAlignment(header, Qt::AlignCenter);
+  auto header = new QLabel(title);
+  header->setObjectName("header");
+  layout->addWidget(header);
+  layout->setAlignment(header, Qt::AlignCenter);
 }
 
-void Page::attachLayout()
-{
-    setLayout(layout);
-}
+void Page::attachLayout() { setLayout(layout); }
