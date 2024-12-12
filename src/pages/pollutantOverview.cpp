adb6f3ae1d6eddbe5a991d3af2303d2fc8a938fe
mov29rr <max.varea@gmail.com>
2024-12-12 10:50:28 +0000

    Added auto scaling graph for pollutants in pollutant overview page

--- src/pages/pollutantOverview.cpp
+++ src/pages/pollutantOverview.cpp
@@ -1,16 +1,44 @@
 #include "pollutantOverview.hpp"
 
-#include <iostream>
+#include <QCompleter>
+#include <QStringListModel>
 
-PollutantOverviewPage::PollutantOverviewPage() : Page("Pollutant Overview Page") {
+#include "database/dbConnection.hpp"
+
+void PollutantSearchHintModel::setFilter(const QString& text) {
+  QSqlQuery query;
+  query.prepare("SELECT LABEL FROM determinand ");
+
+  if (!query.exec()) {
+    std::cerr << "Failed to query db" << std::endl;
+  }
+
+  setQuery(std::move(query));
+}
+
+PollutantOverviewPage::PollutantOverviewPage()
+    : Page("Pollutant Overview Page"),
+      _searchField(new QLineEdit),
+      _contentPlaceholder(new QWidget),
+      _chart(new AutoScalingPollutantContaminationGraph("Pollutant", _complianceLevels)) {
   // create searchbar
   auto searchLabel = new QLabel(tr("Search:"));
-  auto searchField = new QLineEdit();
-  searchLabel->setBuddy(searchField);
+  searchLabel->setBuddy(_searchField);
+  _searchField->setPlaceholderText("Enter a pollutant name");
+
+  auto searchModel = new PollutantSearchHintModel;
+
+  auto completer = new QCompleter(searchModel, this);
+  completer->setCaseSensitivity(Qt::CaseInsensitive);
+
+  _searchField->setCompleter(completer);
+
+  connect(_searchField, &QLineEdit::textChanged, searchModel, &PollutantSearchHintModel::setFilter);
+  connect(_searchField, &QLineEdit::returnPressed, this, &PollutantOverviewPage::onSearch);
 
   auto searchLayout = new QHBoxLayout();
   searchLayout->addWidget(searchLabel);
-  searchLayout->addWidget(searchField);
+  searchLayout->addWidget(_searchField);
 
   content->addLayout(searchLayout);
   content->addSpacing(10);
@@ -28,4 +56,38 @@ PollutantOverviewPage::PollutantOverviewPage() : Page("Pollutant Overview Page")
 
   content->addWidget(pollutantLabel);
   content->addSpacing(10);
+  content->addWidget(_contentPlaceholder);
+}
+
+void PollutantOverviewPage::onSearch() {
+  const QString label = _searchField->text();
+
+  QSqlQuery getDeterminantQuery;
+  getDeterminantQuery.prepare("SELECT ID FROM determinand WHERE LABEL = :label LIMIT 1");
+  getDeterminantQuery.bindValue(":label", label);
+  if (!getDeterminantQuery.exec() || !getDeterminantQuery.next()) {
+    std::cerr << "Failed to find pollutant" << std::endl;
+    return;
+  }
+
+  int id = getDeterminantQuery.value(0).toInt();
+
+  _chart->clear();
+
+  DbConnection db("../database/database.sql");
+  auto records = db.query(
+      "SELECT RESULT, DATE FROM measurement "
+      "WHERE DETERMINAND_ID = :id "
+      "ORDER BY DATE ASC",
+      std::vector<QueryBinding>{{":id", id}});
+
+  std::vector<PollutantContaminationPoint> measurements;
+  for (const auto& record : records) {
+    measurements.emplace_back(record.field("date").value().toDateTime(), record.field("result").value().toReal());
+  }
+
+  _chart->addPoints(measurements);
+
+  content->replaceWidget(_contentPlaceholder, _chart);
+  _contentPlaceholder = _chart;
 }
--- src/pages/pollutantOverview.hpp
+++ src/pages/pollutantOverview.hpp
@@ -1,14 +1,33 @@
 #pragma once
 
-#include <QLineEdit>
-#include <QTextBrowser>
-
 #include "page.hpp"
+#include "core/pollutantContamination.hpp"
+#include "charts/autoScalingPollutantContaminationGraph.hpp"
+
+#include <QLineEdit>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QTextBrowser>
+#include <iostream>
+
+class PollutantSearchHintModel : public QSqlQueryModel {
+  Q_OBJECT
+ public slots:
+  void setFilter(const QString& text);
+};
 
 /**
  * The pollutant overview page.
  */
 class PollutantOverviewPage : public Page {
+  Q_OBJECT
+ private:
+  QLineEdit* _searchField;
+  AutoScalingPollutantContaminationGraph* _chart;
+  QWidget* _contentPlaceholder;
+
+  const ComplianceLevels _complianceLevels{.veryLow = 2, .low = 3, .high = 6, .veryHigh = 8};
+
  public:
   /**
    * Constructs the pollutant overview page.
@@ -16,5 +35,9 @@ class PollutantOverviewPage : public Page {
   PollutantOverviewPage();
 
  private:
+  // TODO
   QWidget* overview() override { return new QWidget; }
+
+ private slots:
+  void onSearch();
 };
